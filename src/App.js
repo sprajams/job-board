ada414f0178c8b38ef61bf020caec972771dc5a3
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-07-05 16:39:51 -0700

    fetch job posting on page load, load more btn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Board from "./components/Board";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Board/index.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+function Board() {
+  const [postIds, setPostIds] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://hacker-news.firebaseio.com/v0/jobstories.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPostIds(data);
+      });
+  }, []);
+
+  const [postData, setPostData] = useState([]);
+  const [numPostDisplay, setNumPostDisplay] = useState(9);
+  // on page load, display first 9 job postings
+  const fetchPostIds = async (data, num) => {
+    if (data.length > 0) {
+      const dataToDisplay = data.slice(0, num);
+      console.log(dataToDisplay);
+      const newStuff = [];
+      for (const x in dataToDisplay) {
+        const awaitData = await fetch(
+          ` https://hacker-news.firebaseio.com/v0/item/${dataToDisplay[x]}.json`
+        ).then((res) => res.json());
+        newStuff.push(awaitData);
+      }
+      setPostData(newStuff);
+    }
+  };
+  useEffect(() => {
+    fetchPostIds(postIds, numPostDisplay);
+  }, [postIds, numPostDisplay]);
+
+  const loadMorePosts = () => {
+    setNumPostDisplay(numPostDisplay + 6);
+  };
+
+  return (
+    <div>
+      <h2>HackerNews Jobs</h2>
+      {postData.length > 0
+        ? postData.map((x, i) => {
+            return (
+              <div key={i}>
+                <div>{i}</div>
+                <div>{x.title}</div>
+              </div>
+            );
+          })
+        : null}
+      {postData.length}
+      <button onClick={loadMorePosts}>load more jobs</button>
+    </div>
+  );
+}
+export default Board;
